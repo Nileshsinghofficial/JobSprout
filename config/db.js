7143b4a5bb264be7d257e02670df036e767be9de
nileshsinghofficial <nigamsingh888@gmail.com>
2024-08-07 09:09:20 +0530

    add local version code work succesfuly

--- config/db.js
+++ config/db.js
@@ -3,20 +3,20 @@ const bcrypt = require('bcryptjs');
 const mysql = require('mysql2');
 require('dotenv').config();
 
-// Create a connection to the MySQL database
-const urlDB = 'mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}'
-const db = mysql.createConnection(urlDB);
+// // Create a connection to the MySQL database
+// const urlDB = 'mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}'
+// const db = mysql.createConnection(urlDB);
 
 
 
 // Create a connection to the MySQL database
-// const db = mysql.createConnection({
-//     host: process.env.DB_HOST,
-//     user: process.env.DB_USERNAME,
-//     password: process.env.DB_PASSWORD,
-//     database: process.env.DATABASE,
-//     port: process.env.DB_PORT
-// });
+const db = mysql.createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DATABASE,
+    port: process.env.DB_PORT
+});
 
 // Handle connection errors
 db.on('error', (err) => {
