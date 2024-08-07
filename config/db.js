e0cbaaf27034680a4989c64c34d193fbf572a134
nileshsinghofficial <nigamsingh888@gmail.com>
2024-08-07 11:39:37 +0530

    succesfuly setup databse with online server

--- config/db.js
+++ config/db.js
@@ -4,25 +4,33 @@ const mysql = require('mysql2');
 require('dotenv').config();
 const fs = require('fs');
 
-// // Create a connection to the MySQL database
-// const urlDB = 'mysql://${process.env.MYSQLUSER}:${process.env.MYSQLPASSWORD}@${process.env.RAILWAY_PRIVATE_DOMAIN}:3306/${process.env.MYSQL_DATABASE}'
-// const db = mysql.createConnection(urlDB);
-
-
-
-// Create a connection to the MySQL database
+// Create a connection to the railway MySQL database
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DATABASE,
-    port: process.env.DB_PORT,
+    host: process.env.MYSQLHOST,
+    user: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
     ssl: {
         ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
         rejectUnauthorized: false
     },
     connectTimeout: 10000 // 10 seconds
 });
+
+// // Create a connection to the local MySQL database
+// const db = mysql.createConnection({
+//     host: process.env.DB_HOST,
+//     user: process.env.DB_USERNAME,
+//     password: process.env.DB_PASSWORD,
+//     database: process.env.DATABASE,
+//     port: process.env.DB_PORT,
+//     ssl: {
+//         ca: fs.readFileSync(path.join(__dirname, 'ca.pem')),
+//         rejectUnauthorized: false
+//     },
+//     connectTimeout: 10000 // 10 seconds
+// });
 
 // Handle connection errors
 db.on('error', (err) => {
