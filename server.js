6ca100e964bb14c986737bd3abb6c47bfb7262b2
nileshsinghofficial <nigamsingh888@gmail.com>
2024-08-07 09:15:56 +0530

    db port update

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ require('dotenv').config();
 
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
 // Import routes
 const authRoutes = require('./routes/auth');
